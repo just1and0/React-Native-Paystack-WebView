d43549ce448410d960ddc49c8ae6d5abc29cf4a0
Mosolasi Akinyemi <akinyemimosolasi@gmail.com>
2021-07-21 19:26:22 +0100

    chore: added helper functions

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,3 +1,7 @@
+const IGNORE = 0;
+const WARN = 1;
+const ERROR = 2;
+
 module.exports = {
   env: {
     browser: true,
@@ -5,7 +9,13 @@ module.exports = {
     jest: true,
     es6: true,
   },
-  extends: ['plugin:react/recommended', 'airbnb'],
+  extends: [
+    'plugin:react/recommended',
+    'airbnb',
+    'plugin:import/warnings',
+    'plugin:import/typescript',
+    'plugin:import/errors',
+  ],
   parser: '@typescript-eslint/parser',
   parserOptions: {
     ecmaVersion: 6,
@@ -14,6 +24,12 @@ module.exports = {
       modules: true,
     },
   },
-  plugins: ['react', 'react-native', '@typescript-eslint'],
-  rules: {},
+  plugins: ['react', 'react-native', '@typescript-eslint', 'import'],
+  rules: {
+    // Allow imports from dev and peer dependencies
+    'import/no-extraneous-dependencies': [ERROR, { devDependencies: true, peerDependencies: true }],
+    'no-unused-vars': IGNORE,
+    'no-shadow': IGNORE,
+    'import/no-unresolved': IGNORE,
+  },
 };
