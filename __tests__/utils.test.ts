f24d5ae645b9e0007de0970e22013013fa019ff2
Oluwatobi Shokunbi <ayoshokz@gmail.com>
2025-01-12 13:23:13 +0000

    Update utils.test.ts

--- __tests__/utils.test.ts
+++ __tests__/utils.test.ts
@@ -68,68 +68,6 @@ describe('Utility functions work properly', () => {
 });
 
 describe('Utility functions work properly', () => {
-  test('getChannels should return a stringified array with commas', () => {
-    let testChannels: PaymentChannels[] = ['card', 'bank'];
-    let expectedOutput = 'channels: ["card","bank"],';
-
-    const result = getChannels(testChannels);
-    expect(result).toBe(expectedOutput);
-  });
-
-  test('toAmountInKobo should return kobo value for amount', () => {
-    let result = toAmountInKobo('4600.00');
-    expect(result).toBe(460000);
-
-    result = toAmountInKobo(1200.93);
-    expect(result).toBe(120093);
-  });
-
-  test('getAmountValueInKobo should return kobo value for valid amounts', () => {
-    let result = getAmountValueInKobo('4600.00');
-    expect(result).toBe(460000);
-
-    result = getAmountValueInKobo('not_correct_money_amount');
-    expect(result).toBe(0);
-  });
-
-  test('isValidDecimalMonetaryValue should return true for only valid amount', () => {
-    let result = isValidDecimalMonetaryValue('2500.00');
-    expect(result).toBe(true);
-
-    result = isValidDecimalMonetaryValue('2500');
-    expect(result).toBe(true);
-
-    result = isValidDecimalMonetaryValue('invalid_amount');
-    expect(result).toBe(false);
-  });
-
-  test('isValidStringAmount should return true for valid amount strings', () => {
-    let result = isValidStringAmount('2500.00');
-    expect(result).toBe(true);
-
-    result = isValidStringAmount('not_money_amount');
-    expect(result).toBe(false);
-
-    result = isValidStringAmount('2500.');
-    expect(result).toBe(false);
-  });
-
-  test('toNumber should convert string amount to number', () => {
-    const result = toNumber('2500.00');
-    expect(result).toBe(2500);
-  });
-
-  test('isNegative should return true if amount is negative', () => {
-    let result = isNegative('-200.00');
-    expect(result).toBe(true);
-
-    result = isNegative(-200.0);
-    expect(result).toBe(true);
-
-    result = isNegative(200.0);
-    expect(result).toBe(false);
-  });
-
   test('buildKeyValueString should return correct string for key-value', () => {
     let result = buildKeyValueString('key1', 'value1');
     expect(result).toBe("key1: 'value1',");
