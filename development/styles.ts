9c179329b7af0d121e7906fe32e4446bf37f6bc8
trapintrovert <mcneo062@gmail.com>
2025-06-03 11:15:44 +0100

    fix(RNPSW):: added paddingTop to the container style to fix modal top padding issue

--- development/styles.ts
+++ development/styles.ts
@@ -1,8 +1,9 @@
 import { StyleSheet } from 'react-native';
 
 export const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        backgroundColor: 'white'
-    },
+  container: {
+    flex: 1,
+    backgroundColor: 'white',
+    paddingTop: 50,
+  },
 });
