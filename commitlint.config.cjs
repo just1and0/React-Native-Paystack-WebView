27481dbe548e7ba34dccdb69259e3709c88d2030
Jokotoye Ademola <jokotoyeademola95@gmail.com>
2025-09-01 17:07:44 +0100

    chore: add commitling config

---
+++ commitlint.config.cjs
@@ -0,0 +1,8 @@
+module.exports = {
+  extends: ['@commitlint/config-conventional'],
+  // We can add custom rules here if needed
+  rules: {
+    // Example: disallow empty scope
+    // 'scope-empty': [2, 'never'],
+  }
+};
